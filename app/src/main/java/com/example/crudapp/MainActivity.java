964da3a3e5cd91d1863dbf9b876093329b9dadc8
Boomika <boomika.gajendiran@rapidcare.ai>
2024-12-20 11:45:24 +0530

    Sixth commit

--- app/src/main/java/com/example/crudapp/MainActivity.java
+++ app/src/main/java/com/example/crudapp/MainActivity.java
@@ -14,7 +14,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    EditText id,name,contact,dob;
+    EditText id,name = this.<EditText>findViewById(R.id.name),contact,dob;
     Button insert,update,delete,view;
     DBHelper dbHelper ;
     @Override
@@ -27,72 +27,59 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        name = (EditText) findViewById(R.id.name);
-        id = (EditText) findViewById(R.id.id);
-        contact = (EditText) findViewById(R.id.contact);
-        dob = (EditText) findViewById(R.id.dob);
-        insert = (Button) findViewById(R.id.btn_insert);
-        update = (Button) findViewById(R.id.btn_update);
-        delete = (Button) findViewById(R.id.btn_delete);
-        view = (Button) findViewById(R.id.btn_view);
+        id = findViewById(R.id.id);
+        contact = findViewById(R.id.contact);
+        dob = findViewById(R.id.dob);
+        insert = findViewById(R.id.btn_insert);
+        update = findViewById(R.id.btn_update);
+        delete = findViewById(R.id.btn_delete);
+        view = findViewById(R.id.btn_view);
         dbHelper = new DBHelper(MainActivity.this);
         view_all();
 
-        insert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                boolean b = dbHelper.insert_data(name.getText().toString(), contact.getText().toString(), dob.getText().toString());
-                if (b) {
-                    Toast.makeText(MainActivity.this, "Data inserted", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "Data not inserted", Toast.LENGTH_SHORT).show();
-                }
-            }
-            });
-
-        update.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                boolean b = dbHelper.update_data(id.getText().toString(), name.getText().toString(), contact.getText().toString(), dob.getText().toString());
-                if (b) {
-                    Toast.makeText(MainActivity.this, "Data updated", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "Data not updated", Toast.LENGTH_SHORT).show();
-                }
+        insert.setOnClickListener(v -> {
+            boolean b = dbHelper.insert_data(name.getText().toString(), contact.getText().toString(), dob.getText().toString());
+            if (b) {
+                Toast.makeText(MainActivity.this, "Data inserted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Data not inserted", Toast.LENGTH_SHORT).show();
             }
         });
 
-        delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Integer i = dbHelper.delete_data(id.getText().toString());
-                if (i > 0){
-                    Toast.makeText(MainActivity.this, "Data deleted", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "Data not deleted", Toast.LENGTH_SHORT).show();
-                }
+        update.setOnClickListener(v -> {
+            boolean b = dbHelper.update_data(id.getText().toString(), name.getText().toString(), contact.getText().toString(), dob.getText().toString());
+            if (b) {
+                Toast.makeText(MainActivity.this, "Data updated", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Data not updated", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        delete.setOnClickListener(v -> {
+            Integer i = dbHelper.delete_data(id.getText().toString());
+            if (i > 0){
+                Toast.makeText(MainActivity.this, "Data deleted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Data not deleted", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     private void view_all() {
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Cursor cursor = dbHelper.view_data();
-                if (cursor.getCount() == 0){
-                    show_Message("Error", "No data found");
-                    return;
-                }
-                StringBuilder buffer = new StringBuilder();
-                while (cursor.moveToNext()){
-                    buffer.append("Id : ").append(cursor.getString(0)).append("\n");
-                    buffer.append("Name : ").append(cursor.getString(1)).append("\n");
-                    buffer.append("Contact : ").append(cursor.getString(2)).append("\n");
-                    buffer.append("Dob : ").append(cursor.getString(3)).append("\n").append("\n");
-                }
-                show_Message( buffer.toString(),"Data");
+        view.setOnClickListener(v -> {
+            Cursor cursor = dbHelper.view_data();
+            if (cursor.getCount() == 0){
+                show_Message("Error", "No data found");
+                return;
             }
+            StringBuilder buffer = new StringBuilder();
+            while (cursor.moveToNext()){
+                buffer.append("Id : ").append(cursor.getString(0)).append("\n");
+                buffer.append("Name : ").append(cursor.getString(1)).append("\n");
+                buffer.append("Contact : ").append(cursor.getString(2)).append("\n");
+                buffer.append("Dob : ").append(cursor.getString(3)).append("\n").append("\n");
+            }
+            show_Message( buffer.toString(),"Data");
         });
     }
 
